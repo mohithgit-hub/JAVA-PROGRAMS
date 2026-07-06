4c5c34cf21cd0e941cf5ac9478117b46e3247947
MOHITH RAJ A K <akmohithraj@gmail.com>
2026-07-06 11:41:33 +0530

    Delete training files directory

--- training files/day 7/src/bubblesort.java
+++
@@ -1,6 +0,0 @@
-public class bubblesort {
-    public static void main(String[] args){
-        
-    }
-
-}
--- training files/day8/src/convertstringtoarray.java
+++
@@ -1,7 +0,0 @@
-public class convertstringtoarray {
-    public static void main(String[] args){
-        String name = "malayalam";
-        char[] arr = name.toCharArray();
-        System.out.println(arr);
-    }
-}
